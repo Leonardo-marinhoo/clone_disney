fe29b34bc3e65f61b36458ea43562c2392f61548
Leo-Marinhoo <leo.ccodes@gmail.com>
2024-08-11 12:12:16 -0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,28 @@ document.addEventListener("DOMContentLoaded", function () {
   const tabs_buttons = document.querySelectorAll("[data-tab-button]");
   const questions = document.querySelectorAll("[data-question]");
 
+  
+  const hero_section = document.querySelector('.hero');
+  const hero_y = hero_section.clientHeight;
+  
+  window.addEventListener("scroll", function (){
+    let logo = document.querySelector(".header__logo");
+    let buy_btn = document.querySelector(".button__buy");
+    let header = document.querySelector(".header");
+
+    if(this.window.scrollY > hero_y){
+      header.classList.remove("header--is-hidden");
+      buy_btn.classList.remove("button--is-hidden");
+      logo.classList.remove("header__logo--is-hidden");
+      
+    }else{
+      header.classList.add("header--is-hidden");
+      buy_btn.classList.add("button--is-hidden");
+      logo.classList.add("header__logo--is-hidden");
+
+    }
+  } )
+
   for (let i = 0; i < tabs_buttons.length; i++) {
     tabs_buttons[i].addEventListener("click", function (e) {
       let target_tab = e.target.getAttribute("data-tab-button");
